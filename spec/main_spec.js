c52aacc10380f4ba9c201d37325df1deee7972ca
lyn <shandonglyn@163.com>
2017-08-15 17:03:00 +0800

    'finish'

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -7,44 +7,43 @@ var expect = chai.expect;
 chai.use(sinonChai);
 
 var getPostNet = require("../lib/main.js");
+// var getBarcode = require("../lib/digit-barcode.js");
 
-describe("beer song", function(){
-    it("No more bottles of beer on the wall", function(){
-        let number = 0;
-        var result = getPostNet(number);
-        var expect_string =``;
+describe("POST-NET", function(){
+    it("Input a postcode which length is 5", function(){
+        let input = '95713';
+        var result = getPostNet(input);
+        var expect_string ='||:|:::|:|:|:::|:::||::||::|:|:|';
         expect(expect_string).to.equal(result);
     });
 
 
-    it("1 bottle of beer on the wall", function(){
-        let number = 1;
-        var result = getPostNet(number);
-        var expect_string =
-            ``;
+    it("Input a postcode which length is 9", function(){
+        let input = '191042345';
+        var result = getPostNet(input);
+        var expect_string = '|:::|||:|:::::||||::::|::|::|:|::||::|::|:|:|::::|||';
         expect(expect_string).to.equal(result);
     });
 
-    it("3 bottles of beer on the wall", function(){
-        let number = 3;
-        var result = getPostNet(number);
-        var expect_string =
-            ``;
+    it("Input a postcode which length is 10", function(){
+        let input = '19104-2345';
+        var result = getPostNet(input);
+        var expect_string = '|:::|||:|:::::||||::::|::|::|:|::||::|::|:|:|::::|||';
         expect(expect_string).to.equal(result);
     });
 
-    it("99 bottles of beer on the wall", function(){
-        let number = 99;
-        var result = getPostNet(number);
-        var expect_string = ``;
+    it("Input a barcode to 5 ", function(){
+        let input = '||:|:::|:|:|:::|:::||::||::|:|:|';
+        var result = getPostNet(input);
+        var expect_string = '95713';
         expect(expect_string).to.equal(result);
     });
 
-    // it("测试用例2", function(){
-    //
-    //     main();
-    //     var result = _.flatten(console.log.args).join("\n");
-    //     var expect_string = '';
-    //     expect(expect_string).to.equal(result);
-    // });
+    it("Input a barcode to 10 ", function(){
+        let input = '|:::|||:|:::::||||::::|::|::|:|::||::|::|:|:|::::|||';
+        var result = getPostNet(input);
+        var expect_string = '19104-2345';
+        expect(expect_string).to.equal(result);
+    });
+
 });
